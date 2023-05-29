ba291d4d6647ed8f5a33f3a683b678fddcd45ba6
Meenadshi <meenadshi20@gmail.com>
2023-05-29 08:18:50 +0530

    -Update.Included MVC architecture for get method, Included the post method

---
+++ controllers/todo_items.js
@@ -0,0 +1,26 @@
+const getALLToDoItems = (req,res)=>{
+    res.send("List of all todo_items")
+  }
+  
+  
+  
+  const createANewToDoItems =(req,res)=>{
+    // res.send("Creating New Items")
+    res.status(200).json(req.body)
+  }
+  
+  const getAToDoItems =(req,res)=>{
+    // res.send("Creating New Items")
+    res.send(`Displaying Item with id ${req.params.id}`)
+  }
+  
+  const updateAtoDoItem =(req,res)=>{
+    // res.send("Creating New Items")
+    res.send(`Updating Item with id ${req.params.id}`)
+  }
+  
+  const deleteATODoItem=(req,res)=>{
+    // res.send("Creating New Items")
+    res.send(`Deleting Item with id ${req.params.id}`)
+  }
+  module.exports={getALLToDoItems,createANewToDoItems,getAToDoItems,updateAtoDoItem,deleteATODoItem}
---
+++ routes/todo_items.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const router=express.Router()
+
+const {getALLToDoItems,createANewToDoItems,getAToDoItems,updateAtoDoItem,deleteATODoItem}=require('../controllers/todo_items')
+router.route("/").get(getALLToDoItems).post(createANewToDoItems);
+// router.route('/').get(createANewToDoItems);
+router.route('/:id').get(getAToDoItems).patch(updateAtoDoItem).delete(deleteATODoItem);
+module.exports=router
